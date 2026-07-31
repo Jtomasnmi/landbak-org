ad9926594e749ab794c70f81aa9c2701c512e290
unknown <jmtomas.nmi@gmail..com>
2026-07-31 11:23:43 +0800

    remove unused packege

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "./Header";
 import ImgCover from "./assets/sheild.png";
 import { Badge, Button } from "flowbite-react";
@@ -24,15 +23,33 @@ const Index = () => {
               AI - driven solutions to help your business scale efficiently,
               improve operations, and achieve top-tier service delivery.
             </p>
-            <Button color="green" className="rounded-none">Read More</Button>
+            <Button color="green" className="rounded-none">
+              Read More
+            </Button>
           </div>
           <img src={ImgCover} alt="" className="aspect-video size-4/12" />
         </div>
         <div className="flex items-center justify-center mt-16 2xl:mt-36 border-t border-olive-300 pt-5 gap-10 2xl:gap-20 2xl:pt-16">
-          <img src={vsa} className="aspect-auto object-center w-32 2xl:w-36 " alt="" />
-          <img src={inky} className="aspect-auto object-center w-32 2xl:w-36 " alt="" />
-          <img src={itGlue} className="aspect-auto object-center w-32 2xl:w-36 " alt="" />
-          <img src={saas} className="aspect-auto object-center w-32 2xl:w-36 " alt="" />
+          <img
+            src={vsa}
+            className="aspect-auto object-center w-32 2xl:w-36 "
+            alt=""
+          />
+          <img
+            src={inky}
+            className="aspect-auto object-center w-32 2xl:w-36 "
+            alt=""
+          />
+          <img
+            src={itGlue}
+            className="aspect-auto object-center w-32 2xl:w-36 "
+            alt=""
+          />
+          <img
+            src={saas}
+            className="aspect-auto object-center w-32 2xl:w-36 "
+            alt=""
+          />
           <img
             src={darkweb}
             className="aspect-auto object-center w-32 2xl:w-36 "
